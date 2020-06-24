fd8266eed74df37032913b12eaf3624db109c3de
pritikmshaw <pritikmshaw18@gmail.com>
2020-06-24 18:56:22 +0530

    Create Main3Activity.java

---
+++ Gujrat/app/src/main/java/com/example/gujrat/Main3Activity.java
@@ -0,0 +1,21 @@
+package com.example.gujrat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    public void register(View view) {
+    }
+
+    public void loginn(View view) {
+    }
+}
