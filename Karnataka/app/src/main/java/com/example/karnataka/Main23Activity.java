5ca925d7643f1070850a90d614371323388f5472
pritikmshaw <pritikmshaw18@gmail.com>
2020-07-05 21:47:46 +0530

    Create Main23Activity.java

---
+++ Karnataka/app/src/main/java/com/example/karnataka/Main23Activity.java
@@ -0,0 +1,14 @@
+package com.example.karnataka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main23Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main23);
+    }
+}
